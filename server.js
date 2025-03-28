91407f13d6a124730e9b897eb7a0dc24d236cc3d
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-03-28 06:38:07 +0200

    feat: Initialized Express server with dynamic route loading and dotenv configuration

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import fs from "node:fs";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+//routes
+const routeFiles = fs.readdirSync("./backend/src/routes");
+
+routeFiles.forEach((file) => {
+  // use dynamic import
+  import(`./backend/src/routes/${file}`)
+    .then((route) => {
+      app.use("/api/v1", route.default);
+    })
+    .catch((err) => {
+      console.log("Failed to load route file", err);
+    });
+});
+
+const server = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to strt server.....", error.message);
+    process.exit(1);
+  }
+};
+
+server();
